d72273715045e7738fbf152633955b6f6e659bd8
ibnsafar <yokubibnsafar@gmail.com>
2022-07-01 14:36:01 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -15,31 +15,3 @@ const App = () => {
     )
 }
 export default App;
-// config = {
-//     method: "post",
-//     url: "https://toko.ox-sys.com/security/auth_check",
-//     headers: {
-//         'Content-type': 'application/x-www-form-urlencoded',
-//         'accept': 'application/json'
-//     },
-//     data: {
-//         "_username": values.username,
-//         "_password": values.password,
-//         "_subdomain": values.subdomain
-//     }
-// }
-
-// const res = axios(config).then((res) => {
-//     console.log(res)
-//     return res;
-// }).catch((err) => {
-//     console.log(err)
-// })
-
-// //if auth is success 
-// if (res.data.status === 201) {
-//     localStorage.setItem("config", config)
-//     navigate("/")
-// } else {
-//     // showing error
-// }
